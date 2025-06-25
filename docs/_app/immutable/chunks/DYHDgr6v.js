5368ad6d3e78ed51208cf473ec505b052dfd386c
Milan Schollier <milan.schollier@gmail.com>
2025-06-25 09:27:11 +0200

    :hammer: spelling mistake

--- docs/_app/immutable/chunks/DYHDgr6v.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_155huq9)==null?void 0:s.base)??"/GPS_obfuscator_thesis";var t;const e=((t=globalThis.__sveltekit_155huq9)==null?void 0:t.assets)??a;export{e as a,a as b};
---
+++ docs/_app/immutable/entry/start.D05PD2fI.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/Ca4Ns7Pj.js";export{t as start};
